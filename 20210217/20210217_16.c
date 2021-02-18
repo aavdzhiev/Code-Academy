5d6ce4857306f68ed74c26ea54070c2d4382abfa
unknown <anastas.avdzhiev@gmail.com>
2021-02-18 16:06:10 +0200

    fix 16

--- 20210217/20210217_16.c
+++ 20210217/20210217_16.c
@@ -12,14 +12,12 @@
 
 void fillRand(int arr[], int len);
 void print_arr(int *a, int len);
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
-}
+int cmpfunc (const void * a, const void * b);
+void copySorted(int *dest, int *src1, int *src2, int lenDest, int lenSrc);
 
 int main() {
 	srand(time(NULL));
 	int arr1[ARR_LEN] = {0}, arr2[ARR_LEN] = {0}, arr3[ARR_LEN * 2] = {0};
-
 
 	printf("Arr 1: \n");
 	fillRand(arr1, ARR_LEN);
@@ -29,10 +27,34 @@ int main() {
 	fillRand(arr2, ARR_LEN);
 	qsort(arr2, ARR_LEN, sizeof(int), cmpfunc);
 	print_arr(arr2, ARR_LEN);
+	printf("\nArr 3: \n");
+	copySorted(arr3, arr1, arr2, ARR_LEN * 2, ARR_LEN);
+	print_arr(arr3, ARR_LEN * 2);
 
+	return 0;
+}
 
+void copySorted(int *dest, int *src1, int *src2, int lenDest, int lenSrc) {
+	int i = 0, j = 0, k = 0;
+
+	while (i < lenSrc && j < lenSrc) {
+		if (src1[i] < src2[j]) {
+			dest[k++] = src1[i++];
+		} else {
+			dest[k++] = src2[j++];
+		}
+	}
+
+	while (i < lenSrc) {
+		dest[k++] = src1[i++];
+	}
+
+	while (i < lenSrc) {
+		dest[k++] = src2[j++];
+	}
 
 }
+
 
 void fillRand(int arr[], int len) {
 	int i;
@@ -46,4 +68,8 @@ void print_arr(int *a, int len) {
 	for (i = 0; i < len; i++) {
 		printf("%d ", a[i]);
 	}
+}
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
 }
