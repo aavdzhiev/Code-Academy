8eaf9fc4f9e3a2c19a40ded0671bb250ecae26d7
unknown <anastas.avdzhiev@gmail.com>
2021-02-18 14:59:18 +0200

    add 9, 14, 16

--- 20210217/20210217_16.c
+++ 20210217/20210217_16.c
@@ -5,3 +5,45 @@
 налага да го сортираме отново.
 Насоки: Проверяваме от кой масив да вземем следващия елемент за
 слагане в масива /цикъл и проверка./  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define ARR_LEN 88
+
+void fillRand(int arr[], int len);
+void print_arr(int *a, int len);
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+int main() {
+	srand(time(NULL));
+	int arr1[ARR_LEN] = {0}, arr2[ARR_LEN] = {0}, arr3[ARR_LEN * 2] = {0};
+
+
+	printf("Arr 1: \n");
+	fillRand(arr1, ARR_LEN);
+	qsort(arr1, ARR_LEN, sizeof(int), cmpfunc);
+	print_arr(arr1, ARR_LEN);
+	printf("\nArr 2: \n");
+	fillRand(arr2, ARR_LEN);
+	qsort(arr2, ARR_LEN, sizeof(int), cmpfunc);
+	print_arr(arr2, ARR_LEN);
+
+
+
+}
+
+void fillRand(int arr[], int len) {
+	int i;
+	for (i = 0; i < len; i++) {
+		arr[i] = rand() % 100;
+	}
+}
+
+void print_arr(int *a, int len) {
+	int i;
+	for (i = 0; i < len; i++) {
+		printf("%d ", a[i]);
+	}
+}
