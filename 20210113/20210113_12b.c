a7f41ef92cb0d5ae663b40eab5b5892765db2f8a
unknown <anastas.avdzhiev@gmail.com>
2021-03-07 23:10:44 +0200

    put print patterns in functions

--- 20210113/20210113_12b.c
+++ 20210113/20210113_12b.c
@@ -9,31 +9,34 @@
 */
 #include <stdio.h>
 
-void printPatterns();
+void print_triangle()
+{
+  int i, j;
+  int spaces = 0;
+  int hashtags = 1;
+
+  for (i = 0; i < 6; i++)
+  {
+
+    for (j = spaces; j <= 11; j++)
+    {
+      printf(" ");
+    }
+    for (j = hashtags; j >= 1; j--)
+    {
+      printf("# ");
+    }
+
+    hashtags += 2;
+    spaces += 2;
+    printf("\n");
+  }
+};
 
 int main()
 {
+  print_triangle();
 
-    int i, j;
-    int spaces = 0;
-    int hashtags = 1;
 
-    for (i = 0; i < 6; i++)
-    {
-
-        for (j = spaces; j <= 11; j++)
-        {
-            printf(" ");
-        }
-        for (j = hashtags; j >= 1; j--)
-        {
-            printf("# ");
-        }
-
-        hashtags += 2;
-        spaces += 2;
-        printf("\n");
-    }
-
-    return 0;
+  return 0;
 }
--- 20210113/20210113_12c.c
+++ 20210113/20210113_12c.c
@@ -14,49 +14,52 @@
 */
 #include <stdio.h>
 
-void printPatterns();
+void print_rhombus()
+{
+  int i, j;
+  int spaces = 0;
+  int hashtags = 1;
+
+  for (i = 0; i < 6; i++)
+  {
+
+    for (j = spaces; j <= 11; j++)
+    {
+      printf(" ");
+    }
+    for (j = hashtags; j >= 1; j--)
+    {
+      printf("# ");
+    }
+
+    hashtags += 2;
+    spaces += 2;
+    printf("\n");
+  }
+
+  spaces = 3;
+  hashtags = 9;
+  for (i = 0; i < 6; i++)
+  {
+
+    for (j = spaces; j >= 0; j--)
+    {
+      printf(" ");
+    }
+    for (j = hashtags; j >= 1; j--)
+    {
+      printf("# ");
+    }
+
+    hashtags -= 2;
+    spaces += 2;
+    printf("\n");
+  }
+}
 
 int main()
 {
+  print_rhombus();
 
-    int i, j;
-    int spaces = 0;
-    int hashtags = 1;
-
-    for (i = 0; i < 6; i++)
-    {
-
-        for (j = spaces; j <= 11; j++)
-        {
-            printf(" ");
-        }
-        for (j = hashtags; j >= 1; j--)
-        {
-            printf("# ");
-        }
-
-        hashtags += 2;
-        spaces += 2;
-        printf("\n");
-    }
-    spaces = 3;
-    hashtags = 9;
-    for (i = 0; i < 6; i++)
-    {
-
-        for (j = spaces; j >= 0; j--)
-        {
-            printf(" ");
-        }
-        for (j = hashtags; j >= 1; j--)
-        {
-            printf("# ");
-        }
-
-        hashtags -= 2;
-        spaces += 2;
-        printf("\n");
-    }
-
-    return 0;
+  return 0;
 }
