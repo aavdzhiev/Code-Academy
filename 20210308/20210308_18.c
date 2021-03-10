5c7200f7f1ae2081fcd2432ac9a58cbedee78a7a
unknown <anastas.avdzhiev@gmail.com>
2021-03-10 08:10:35 +0200

    fix task 18

--- 20210308/20210308_18.c
+++ 20210308/20210308_18.c
@@ -7,15 +7,41 @@ char gender;
 }t_person;
  в XML формат по показания в лекцията начин. */
 #include <stdio.h>
+static const char *XML_FORMAT_PERSON_OUT = "<person>\n\t\t<name>%s</name>\n\t\t<age>%d</age>\n\t\t<gender>%c</gender>\n</person>";
+static const char *XML_FORMAT_PERSON_IN = "<person>\n\t\t<name>%s/name>\n\t\t<age>%d</age>\n\t\t<gender>%c</gender>\n</person>";
+
 
 typedef struct Person{
 char name[20];
 int age;
 char gender;
-} t_person;
+} person;
 
 int main(void) {
+	person michael = {"Michael", 23, 'M'};
+	person john;
+	char *filename = "task18.xml";
+	FILE *fp = fopen(filename, "w+");
+
+	if (NULL == fp) {
+		printf("Cannot open file.\n");
+	}
+
+	fprintf(fp, XML_FORMAT_PERSON_OUT, michael.name, michael.age, michael.gender);
+
+	fclose(fp);
+
+	/* open another file to test reading of XML */
+	fp = fopen("task18in.xml", "r");
+	if (NULL == fp) {
+		printf("Cannot open file.\n");
+	}
 	
+	fscanf(fp, XML_FORMAT_PERSON_IN, john.name, &john.age, &john.gender);
+	fclose(fp);
+
+	printf("John: %s, %d, %c\n", john.name, john.age, john.gender);
+
 
 	return 0;
 }
