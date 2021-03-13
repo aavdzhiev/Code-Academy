30cf562afdef24c3536b8fa813a53600184c19a7
unknown <anastas.avdzhiev@gmail.com>
2021-03-13 19:48:08 +0200

    fix task 1

--- 20210313/20210313_1.c
+++ 20210313/20210313_1.c
@@ -2,44 +2,83 @@
 изтрива елемент от списък по позиция в списъка. Използвайте следния
 елемент на динамичния списък: */
 #include <stdio.h>
+#include <stdlib.h>
 
-extern node_st *start;
-
-typedef struct node
+typedef struct Node
 {
     int data;
-    node_st *next;
+    struct Node *next;
 } node_st;
 
-void init()
-{
-    start = NULL;
-};
 
-int add(int data)
-{
-    node_st *p = start;
-    start = (node_st *)malloc(sizeof(node_st));
-    if (!start)
-        return -1;
+void add(node_st ** head_ref, int new_data);
+void deleteNode(node_st **head_ref, int position);
+void display(node_st *node);
 
-    start->data = data;
-    start->next = p;
+int main() {
+    node_st *head = NULL;
+
+    add(&head, 1);
+    add(&head, 3);
+    add(&head, 9);
+    add(&head, 27);
+    add(&head, 54);
+    add(&head, 108);
+
+    printf("Print the list: \n");
+    display(head);
+    deleteNode(&head, 3);
+    printf("\nPrint the list after deletion: \n");
+    display(head);
 
     return 0;
 }
 
-int del_first(int *n)
-{
-    node_st *p = start;
-    if (start)
-    {
-        *n = start->data;
-        start = start->next;
-        free(p);
-
-        return 1;
-    }
-    else
-        return 0;
+void add(node_st ** head_ref, int new_data) 
+{ 
+    node_st *new_node = (node_st *) malloc(sizeof(node_st)); 
+    new_node->data  = new_data; 
+    new_node->next = (*head_ref); 
+    (*head_ref)    = new_node; 
 }
+
+void deleteNode(node_st **head_ref, int position) 
+{ 
+   if (*head_ref == NULL) /* check if linked list is empty */
+      return; 
+  
+   node_st *temp = *head_ref; /* head node */
+  
+    if (position == 0) 
+    { 
+        *head_ref = temp->next;   /* Change head */
+        free(temp);               /* free old head */ 
+        return; 
+    } 
+  
+    /* Find previous node of the node to be deleted */
+    for (int i=0; temp != NULL && i < position - 1; i++) 
+         temp = temp->next; 
+  
+    /* If position is more than number of nodes */ 
+    if (temp == NULL || temp->next == NULL) 
+         return; 
+  
+    /* Node temp->next is the node to be deleted */
+    /* Store pointer to the next of node to be deleted */
+    node_st *next = temp->next->next; 
+  
+    /* Unlink the node from linked list */
+    free(temp->next);
+  
+    temp->next = next;  /* Unlink the deleted node from list */ 
+}
+
+void display(node_st *node)
+{ 
+    while (node != NULL) 
+    { 
+        printf(" %d ", node->data); 
+        node = node->next; 
+    } 
+} 
